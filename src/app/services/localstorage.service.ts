e6a2d0d6e033e2357a29009c53d3906944469555
theuslefone <matheuskeeven22@gmail.com>
2024-03-22 19:31:01 -0300

    Update grid

--- src/app/services/localstorage.service.ts
+++ src/app/services/localstorage.service.ts
@@ -23,18 +23,7 @@ export class LocalStorageService {
      return JSON.parse(savedLayout);
     } else {
       return [
-        { cols: 2, rows: 1, y: 1, x: 1 },
-        { cols: 2, rows: 2, y: 0, x: 2 },
-        { cols: 1, rows: 1, y: 0, x: 4 },
-        { cols: 3, rows: 2, y: 1, x: 4 },
-        { cols: 1, rows: 1, y: 4, x: 5 },
-        { cols: 1, rows: 1, y: 2, x: 1 },
-        { cols: 2, rows: 2, y: 5, x: 5 },
-        { cols: 2, rows: 2, y: 3, x: 2 },
-        { cols: 2, rows: 1, y: 2, x: 2 },
-        { cols: 1, rows: 1, y: 3, x: 4 },
-        { cols: 1, rows: 1, y: 0, x: 6 },
-        { cols: 4, rows: 1, y: 0, x: 6 }
+        { cols: 3, rows: 2, y: 2, x: 3 },
       ];
     }
   }
