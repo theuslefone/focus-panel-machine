6f6529584dac75e318ef6396cacffd3bd1c8e716
theuslefone <matheuskeeven22@gmail.com>
2024-04-23 12:10:30 -0300

    Configura fetch

--- src/app/services/dashboard/dashboard.service.ts
+++ src/app/services/dashboard/dashboard.service.ts
@@ -9,36 +9,57 @@ export class DashboardService {
 
   private async request(method: string, url: string, data?: any): Promise<any> {
     try {
-      const response = await fetch(url, {
-        method,
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-      });
+        const response = await fetch(url, {
+            method,
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: data.dashboard
+        });
 
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        return response;
+    } catch (error) {
+        throw error;
+    }
+} 
+  async saveData(idClient: string, idClp: string, data: any): Promise<any> {
+    const url = `${this.apiUrl}/${idClient}/${idClp}`;
+    const { dashboard } = data;
+  
+    const requestData = {
+      dashboard: JSON.stringify(dashboard)
+    };
+  
+    return await this.request('PUT', url, requestData);
+  }
+  
+  
+
+  private async getRequest(url: string): Promise<any> {
+    try {
+      const response = await fetch(url);
+  
       if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
       }
-
+  
       return await response.json();
     } catch (error) {
       throw error;
     }
   }
 
-  async saveData(idClient: string, idClp: string, data: any): Promise<any> {
-    const url = `${this.apiUrl}/${idClient}/${idClp}`;
-    return await this.request('PUT', url, data);
-  }
-
   async getData(idClient: string, idClp: string): Promise<any> {
     const url = `${this.apiUrl}/${idClient}/${idClp}`;
-    return await this.request('GET', url);
+    return await this.getRequest(url);
   }
 
   async saveDashboard(idClient: string, idClp: string, dashboard: any): Promise<any> {
-    return await this.saveData(idClient, idClp, { "dashboard": dashboard });
+    return await this.saveData(idClient, idClp, { dashboard });
   }
 
   async getDashboard(idClient: string, idClp: string): Promise<any> {
