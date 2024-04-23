0f4b1e36b1900d5a7801059d211e1bbd0f01748e
theuslefone <matheuskeeven22@gmail.com>
2024-04-23 01:48:10 -0300

    Altera rota inicial para cliente 3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home/1/1', pathMatch: 'full' },
+  { path: '', redirectTo: '/home/3/1', pathMatch: 'full' },
   { path: 'home/:idClient/:idClp', component: HomePageComponent },
 ];
 
