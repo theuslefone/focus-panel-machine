cf8672c0a6deb4baca1e3bb5dde9d04deb9779dd
theuslefone <matheuskeeven22@gmail.com>
2024-04-23 09:39:55 -0300

    Atualiza de axios pra fetch

--- src/app/services/dashboard/dashboard.service.ts
+++ src/app/services/dashboard/dashboard.service.ts
@@ -1,38 +1,44 @@
 import { Injectable } from '@angular/core';
-import axios, { AxiosResponse } from 'axios';
-import { Observable } from 'rxjs';
-
 @Injectable({
   providedIn: 'root'
 })
 export class DashboardService {
- 
   private apiUrl = 'http://webcaseiot.com.br:3000/api/dashboard';
 
   constructor() { }
 
   private async request(method: string, url: string, data?: any): Promise<any> {
     try {
-      const response: AxiosResponse = await axios({ method, url, data });
-      return response.data;
+      const response = await fetch(url, {
+        method,
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      return await response.json();
     } catch (error) {
       throw error;
     }
   }
 
   async saveData(idClient: string, idClp: string, data: any): Promise<any> {
-    console.log(data);
     const url = `${this.apiUrl}/${idClient}/${idClp}`;
-    return await this.request('put', url, data);
+    return await this.request('PUT', url, data);
   }
 
   async getData(idClient: string, idClp: string): Promise<any> {
     const url = `${this.apiUrl}/${idClient}/${idClp}`;
-    return await this.request('get', url);
+    return await this.request('GET', url);
   }
 
   async saveDashboard(idClient: string, idClp: string, dashboard: any): Promise<any> {
-    return await this.saveData(idClient, idClp, {"dashboard": dashboard});
+    return await this.saveData(idClient, idClp, { "dashboard": dashboard });
   }
 
   async getDashboard(idClient: string, idClp: string): Promise<any> {
